5bfbb0ad9cfcb030b113a8933625e3460fafce37
Jelani Thompson <jelani.thompson@outlook.com>
2017-03-09 22:37:46 -0500

    Minnie Mouse!

---
+++ projects/javascript/Minnie.js
@@ -0,0 +1,50 @@
+// Let's draw Minnie Mouse!
+var draw = function() {
+    // Head and ears
+    fill(0, 0, 0);
+    ellipse(100, 100, 50, 50);
+    
+    fill(0, 0, 0);
+    ellipse(200, 100, 50, 50);
+    
+    fill(0, 0, 0);
+    ellipse(150, 150, 100, 100);
+    
+    // Bow tie
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(170, 100, 50, 40);
+    
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(130, 100, 50, 40);
+    
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(130, 80, 40, 40);
+    
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(170, 80, 40, 40);
+    
+    // Polka Dots
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(170, 80, 10, 10);
+    
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(130, 80, 10, 10);
+    
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(130, 100, 10, 10);
+    
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(150, 90, 10, 10);
+    
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(175, 100, 10, 10);
+};
